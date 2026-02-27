73d970bf173f60b991caeccb1cfcc837f6ad68bc
soham178 <sohamsarkar552@gmail.com>
2026-02-27 14:04:21 +0530

    add hero section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Navbar color change on scroll
+
 window.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
@@ -6,6 +7,7 @@ window.addEventListener("scroll", () => {
 
 
 // Toggle Menu
+
 const toggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
@@ -13,3 +15,33 @@ toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 })
 
+
+// Typing Animation
+
+const words = ["Web Developer", "IT Student", "Full Stack Learner"]
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+const typingElement = document.querySelector(".typing");
+
+function type() {
+    currentWord = words[i];
+
+    if (isDeleting) {
+        typingElement.textContent = currentWord.substring(0, j--);
+    } else {
+        typingElement.textContent = currentWord.substring(0, j++);
+    }
+
+    if (!isDeleting && j === currentWord.length) {
+        setTimeout(() => isDeleting = true, 1000);
+    } else if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % words.length;
+    }
+
+    setTimeout(type, isDeleting ? 60 : 120);
+}
+
+type()
