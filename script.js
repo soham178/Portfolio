2e78fc2596e43c9cddf6e35ac608710ae7d802de
soham178 <sohamsarkar552@gmail.com>
2026-02-27 12:50:28 +0530

    add comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Navbar color change on scroll
 window.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
 })
 
 
+// Toggle Menu
 const toggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
